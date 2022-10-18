7a239c4e93e8c25bd6c412b496ece008d163f76c
Jon <Jon@Minettes-MacBook-Pro.local>
2022-10-18 14:49:30 -0400

    custome url elements for custon search url

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let posterID = document.getElementById("Poster")
+//Script to pick movie that will welcome you 
 
 const APIKey = "42f640bb5d98e41dd2c5c48527c1b3fd"
 var welcomeQueryURL = "https://api.themoviedb.org/3/movie/popular?api_key=" + APIKey + "&language=en-US&page=1"
 
 
- 
+//fetching 20 popular movies
 
 fetch(welcomeQueryURL)
     .then(function (responce){
@@ -55,5 +55,41 @@ fetch(welcomeQueryURL)
                 })            
     })
 
+// Form objects with url values
+const genre ={
+    Comedy:"&with_genres=35",
+    Drama:"&with_genres=18",
+    Horror:"&with_genres=27",
+    Sci-Fi:"&with_genres=878",
+    Action:"&with_genres=28",
+    Family:"&with_genres=14",
+    Documentary:"&with_genres=99",
+}
 
+const StreamingService = {
+    Netflix:"&with_watch_providers=8",
+    Hulu:"&with_watch_providers=15",
+    HBOMax:"&with_watch_providers=384",
+    Shudder:"&with_watch_providers=99",
+    Disney+:"&with_watch_providers=377|390",
+    AppleTV+:"&with_watch_providers=350",
+    PrimeVideo:"&with_watch_providers=9|119",
+}
+
+const rating ={
+    G:"certification_country=US&certification=G",
+    PG:"certification_country=US&certification=PG",
+    PG-13:"certification_country=US&certification=PG-13",
+    R:"certification_country=US&certification=R",
+}
+
+let customFormResults = []
+
+let formQueryURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + APIKey +
+                   "&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1" + 
+                   customFormResults
    
+   
+    function formSearch(){
+
+   }
