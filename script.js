4a5f41f181deb549e87f46f0c15822079f77676e
Jon <Jon@Minettes-MacBook-Pro.local>
2022-10-19 16:07:40 -0400

    added comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let posterID = document.getElementById("Poster");
 
+//API Key for Movie Database
 const APIKey = "42f640bb5d98e41dd2c5c48527c1b3fd";
+//Query Url for the welcome movie
 var welcomeQueryURL =
   "https://api.themoviedb.org/3/movie/popular?api_key=" +
   APIKey +
   "&language=en-US&page=1";
 
+//Calling the movie dadbase API > returning just the results > getting movie ID for mor indepth search
 fetch(welcomeQueryURL)
   .then(function (responce) {
     return responce.json();
@@ -18,6 +20,7 @@ fetch(welcomeQueryURL)
       movieresults[Math.floor(Math.random() * movieresults.length)];
     let movieID = String(randommovie?.id);
 
+//url call for movie that will be displayed
     let selectedMovieURL =
       "https://api.themoviedb.org/3/movie/" +
       movieID +
@@ -25,6 +28,7 @@ fetch(welcomeQueryURL)
       APIKey +
       "&language=en-US&append_to_response=release_dates,watch/providers";
 
+// fetching dada from Movie database      
     fetch(selectedMovieURL)
       .then(function (responce) {
         return responce.json();
@@ -32,12 +36,14 @@ fetch(welcomeQueryURL)
       .then(function (data) {
         console.log(data);
 
+//parcing and creating poster url for display
         let posterPath = data.poster_path;
         console.log(posterPath);
         let selectedMoviePoster =
           "https://image.tmdb.org/t/p/w500" + posterPath;
         console.log(selectedMoviePoster);
 
+//parcing movie data(poster img, title, synopsis, genre, runtime) to be displayed on site        
         let posterIMG = document.createElement("img");
         posterIMG.src = selectedMoviePoster;
         document.getElementById("poster").append(posterIMG);
@@ -60,17 +66,22 @@ fetch(welcomeQueryURL)
       });
   });
 
+//Main form functionality made to generate a custom URL for API to parse data and display on site all from clicking a button 
+
 $("#findMovie").click(function (event) {
   event.preventDefault();
+
+//emptying previvously dynamicly generated divs
   $("#poster").empty();
   $("#movieTitle").empty();
   $("#shortSynop").empty();
 
+//Scrolling to top of page to see result  
   $('html, body').animate({
     scrollTop: $("#TopofPage").offset().top
     }, 1000);
 
-  //streaming
+  //looking through streaming service div and retrieving the values of checked boxes to store in custom streaming service string
   let streamTest = "";
   let inputs = $("#searchStreamingService input:checked");
 
@@ -82,7 +93,7 @@ $("#findMovie").click(function (event) {
   streamChoices = streamTest.replace("|", "");
   console.log(streamChoices);
 
-  // genre
+  // looking through genre div and retrieving the values of checked boxes to store in custom genre string
 
   let genreTest = "";
   let genreinputs = $("#searchGenre input:checked");
@@ -95,7 +106,7 @@ $("#findMovie").click(function (event) {
   genreChoices = genreTest.replace(",", "");
   console.log(genreChoices);
 
-  //rating
+  //looking through Rating div and retrieving the values of checked boxes to store in custom rating string
 
   let ratingTest = "";
   let ratinginputs = $("#searchRatings input:checked");
@@ -108,10 +119,12 @@ $("#findMovie").click(function (event) {
   ratingChoices = ratingTest.replace("|", "");
   console.log(ratingChoices);
 
+  // combining the custom genre, streaming, and rating strings into one string varible
   let customFormResults = ratingChoices + genreChoices + streamChoices;
 
   console.log(customFormResults);
 
+  // pluging in custom results into form Query template
   let formQueryURL =
     "https://api.themoviedb.org/3/discover/movie?api_key=" +
     APIKey +
@@ -120,6 +133,7 @@ $("#findMovie").click(function (event) {
 
   console.log(formQueryURL);
 
+//using the finished form query template to get data from API to be parsed
   fetch(formQueryURL)
     .then(function (responce) {
       return responce.json();
@@ -128,6 +142,8 @@ $("#findMovie").click(function (event) {
         console.log(data.results)
       return data.results;
     })
+
+//Calling the movie dadbase API > returning just the results > getting movie ID for more indepth search
     .then(function (movieresults) {
       let randommovie =
         movieresults[Math.floor(Math.random() * movieresults.length)];
@@ -147,6 +163,7 @@ $("#findMovie").click(function (event) {
         .then(function (data) {
           console.log(data);
 
+//parcing and creating poster url for display
           let posterPath = data.poster_path;
           console.log(posterPath);
           let selectedMoviePoster =
@@ -157,6 +174,7 @@ $("#findMovie").click(function (event) {
           posterIMG.src = selectedMoviePoster;
           document.getElementById("poster").append(posterIMG);
 
+//parcing movie data(title, synopsis, genre, runtime) to be displayed on site 
           let title = document.createElement("h1");
           title.textContent = data.title;
           document.getElementById("movieTitle").append(title);
